ac1632723a9a877dc385799fe49f049bc7782fcb
ashlex <angeldust.ru@gmail.com>
2015-11-10 13:26:07 +0300

    Не доехало

---
+++ TrainModeling/TrainModeling/Road/IRoadNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainModeling
+{
+	public interface IRoadNode:IComponent
+	{
+		event EventHandler StateChanged;
+		int TimeOfChange { get; set; }
+
+		void ChangeState();
+	}
+}
